c2d6d99878603c69e30f61609e0be91917198524
Mallarden <antonanderson.com@gmail.com>
2021-02-02 21:54:00 +0100

    latest

--- template-parts/home.php
+++ template-parts/home.php
@@ -12,7 +12,7 @@ get_header(); ?>
     <div class="hero-title">
     <h1>Electro Drain är det moderna och trygga sättet att dränera</h1>
             <h3 class="text-center">━ Ett självklart val</h3>
-            <div class="container pt-3">
+            <!-- <div class="container pt-3">
               <div class="row">
               <div class="col small-6">
               <button button class="button-1"><?php echo do_shortcode('[video_lightbox_youtube video_id="yGKKuLrlkjA" width="640" height="480" anchor="Se oss på TV4"]');?></button>
@@ -21,7 +21,7 @@ get_header(); ?>
                 <button type="button" onclick="location.href='https://www.electrodrain.se/var-teknik/'" class="button-2">Vår Teknik</button>
               </div>
               </div>
-            </div>
+            </div> -->
     </div>
     <div class="arrow-scroll">
         <div class="scroller"></div>
