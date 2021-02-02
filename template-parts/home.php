f43ad70b1b612d5749161c69d2ae49185aeb51f5
Mallarden <antonanderson.com@gmail.com>
2021-02-02 22:16:52 +0100

    latest

--- template-parts/home.php
+++ template-parts/home.php
@@ -33,7 +33,114 @@ get_header(); ?>
         <div class="scroller"></div>
       </div>
       <div class="first-section-wrapper">
-    
+    <section class="first-section">
+      <div class="section-wrapper row">
+        <div class="container">
+          <div class="col-sm-12 col-lg-12 fade-y" data-delighter="start:0.90;">
+            <div class="first-section-text">
+              <h1>Säker dränering - med etablerad teknik </h1>
+              <h3>Electro Drains lösning för torrläggning av fastighetskällare
+                  baseras på en avancerad elektropuls teknik som laddar
+                  fukten i fundamenten och trycker ut den ur byggnationen</h3>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="container emblem-container">
+        <div class="row">
+          <div class="col small-12 large-6">
+          <img class="intro-emblems" src="<?php echo get_stylesheet_directory_uri(); ?>/img/produktgaranti.png" />
+          <p>Vi har stresstestat systemet för 30 år och lämnar därför en 10 års garanti på all teknik.</p>
+            <a href="https://www.electrodrain.se/trygghet/">
+            <button class="emblem-btn" type="button">Läs mer</button>
+            </a>
+          </div>
+          <div class="col small-12 large-6">
+          <img class="intro-emblems" style="background-color:white;" src="<?php echo get_stylesheet_directory_uri(); ?>/img/90dagar-garanti.png" />  
+          <p>Vi lämnar en 90 dagars garanti på att vi nått utlovat resultat annars har ni rätt att få pengarna tillbaka.</p>
+            <a href="https://www.electrodrain.se/trygghet/">
+            <button class="emblem-btn" type="button">Läs mer</button>
+            </a>
+          </div>
+        </div>
+        </div>
+        <div class="how-it-works" style="max-width: 1900px; margin:-15px auto;">
+        <h1 style="margin-top:30px;">Hur fungerar det?</h1>
+        <!-- <img class="" style="background-color:white; max-width: 1900px;" src="<?php echo get_stylesheet_directory_uri(); ?>/img/asset_3D-house_v4.jpg" />   -->
+        <?php echo do_shortcode('[video id=3129]'); ?>
+      </div>
+      <div class="container three-part" style="text-align:center;">
+      <h1>3 fördelar med Electro Drain:</h1>
+        <div class="row">
+          
+        <div class="col large-4">
+          <h3>1. Gräv inte upp trädgården</h3>
+          <p>Med Electro Drains dränering slipper du gräva upp trädäcket, växthuset, stenläggning och/eller gräsmattan.</p>
+        </div>
+        <div class="col large-4">
+          <h3>2. Enkelt, tryggt och snabbt</h3>
+          <p>Normalt tar det en arbetsdag att installera Electro Drains system, sedan är det bara att vänta.</p>
+        </div>
+        <div class="col large-4">
+          <h3>3. Stoppar fukten under huset</h3>
+          <p>Eftersom arbetet görs inifrån stoppar vi även fukten rakt under huset, där grävmaskinen aldrig kommer åt.</p>
+        </div>
+        </div>
+      </div>
+      <? the_content();?>
+      <div class="logo-area">
+        <h1 style="text-align:center; padding-top:25px;">Några nöjda kunder</h1>
+        <?php echo do_shortcode('[gs_logo title="yes" posts="15" order="ASC"  mode="horizontal" speed="1000" inf_loop="0" ticker="1" logo_color="gray_to_def"]'); ?>
+      </div>
+      <div class="category-images">
+        <div class="left-image">
+        <a href="https://www.electrodrain.se/privata-fastigheter/">
+          <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/brand_FACKMAN.jpg" />
+          </a>
+        </div>
+        <div class="right-image">
+       <a href="https://www.electrodrain.se/fastighetsbestand/">
+      <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/vit-parkering.jpg" />
+      </a>  
+    </div>
+      
+      </div>
+      <div class="grey-text">
+        <h3>Så funkar vår teknik</h3>
+        <p>
+        Electro Drains lösning för torrläggning av källare baserar sig på elektroosmos som är en väl beprövad metod för att driva ut fukt ur betong. Eftersom tekniken verkar i själva betongen är den speciellt effektiv för att motverka vattenintrång orsakad av uppstigande, kapillär, markfukt. Att det är en effektiv metod är omvittnat i tester och forskningsrapporter samt, inte minst, i alla de över 3.000 installationer som är gjorda bara i Sverige. Varje installation sker på ett fackmannamässigt sätt med garanterat resultat inom 90 dagar och 10 års funktionsgaranti.
+        </p>
+        <a href="https://www.electrodrain.se/fragor-svar/">
+        <button class="questions-btn" type="button">Vanliga frågor och svar</button>
+        </a>
+      </div>
+      <div class="studier">
+        <h1>Studier</h1>
+        <div class="studier-grey-area">
+          <div class="studier-grey-area-wrapper">
+          <div class="left-area">
+              <div class="studier-container">
+                <img class="intro-emblems" src="<?php echo get_stylesheet_directory_uri(); ?>/img/ChalmersU_black.png" />
+                <h3>Stephan Mangold på Chalmers Industriteknik har gjort en utvärdering och rapport kring vår uttorkningsteknik.</h3>
+                <a href="http://www.electrodrain.se/wp-content/uploads/2021/01/Chalmersrapport-Electro-Drain-Mars-20.pdf" target="_blank">
+                  <button class="studier-btn" type="button">Läs mer</button>
+                </a>
+              </div>
+          </div>
+          <div class="right-area">
+              <div class="studier-container">
+                <img class="intro-emblems" src="<?php echo get_stylesheet_directory_uri(); ?>/img/byggforsk-logga1.png" />
+                <h3>Norska Byggforsk har arbetat fram omfattande studie kring
+kopplingen emellan fuktbekämpning och elektroosmos tekniken.</h3>
+                <a href="http://www.electrodrain.se/wp-content/uploads/2021/01/byggforskrapport_Electro-Drain.pdf" target="_blank">
+                  <button class="studier-btn" type="button">Läs mer</button>
+                </a>
+              </div>
+          </div>
+        </div>
+        </div>
+      </div>
+    </section>
     </div>
 </div><!-- #primary -->
 
